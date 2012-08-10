ff0c7e9fe3e7ac3a0f417a1a8b60693ebdeb3943
Eric Stewart <estewarttt@gmail.com>
2012-08-09 22:10:26 -0400

    Switched over to SFML

--- 4X/4X_main.cpp
+++ 4X/4X_main.cpp
@@ -1,35 +1,62 @@
 #include <iostream>
 #include <fstream>
-#include "4X_func.h"
-#include "SDL_image.h"
-#include <stdlib.h>
-#include <SDL/SDL.h>
+#include "4x_func.h"
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
 using namespace std;
-const int SCREEN_WIDTH = 600;
-const int SCREEN_HEIGHT = 600;
+using namespace sf;
 
-int main(int argc, char *argv[]){ //don't delete the stuff in the brackets, SDL needs it
-    freopen( "CON", "w", stdout );//necessary to display console output
-    freopen( "CON", "w", stderr );//causes weirdness at the end of execution
-    SDL_Init(SDL_INIT_EVERYTHING);
+const int SCREEN_WIDTH = 800; //constant parameters
+const int SCREEN_HEIGHT = 800;
 
-    SDL_Surface* hello = NULL;
-    SDL_Surface* screen = NULL;
+const float speed = 10.f;
+int x = 0, y = 0;
 
-    screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32, SDL_SWSURFACE);
-    hello = IMG_Load("img\\hello world.png");
+int main(){
+    WindowSettings settings;
 
-    if(!hello) //check that the load worked
-        cout<<"error";
+    settings.DepthBits = 24; // Request a 24 bits depth buffer
+    settings.StencilBits = 8;  // Request a 8 bits stencil buffer
+    settings.AntialiasingLevel = 2;  // Request 2 levels of antialiasing
 
-    SDL_BlitSurface(hello, NULL, screen, NULL); //draw image
-    SDL_Flip(screen);
+    Window screen(VideoMode(SCREEN_WIDTH,SCREEN_HEIGHT,32), "Hello OpenGL!", Style::Close, settings); //set up screen
 
-    SDL_Delay(2000);
+    screen.UseVerticalSync(false); //I hate vsync
+    screen.SetFramerateLimit(60);
 
-    SDL_FreeSurface(hello); //closing down
-    SDL_Quit();
+    glClearColor(0.0, 0.0, 0.0, 0.0); //when clearing, use black
+    glClearDepth(1.0); //set clear depth for later use
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //clear GL screen
 
+    glColor3f(1.0, 1.0, 1.0); //when drawing, use white
+    glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0); //set up a rendering persepctive
+
+    while (screen.IsOpened()){
+        Event event;
+        //const Input &input = screen.GetInput();
+        while (screen.GetEvent(event)){
+            if (event.Type == Event::Closed) //user clicked x in top right
+                screen.Close();
+            if ((event.Type == Event::KeyPressed) && (event.Key.Code == Key::Escape)) //user hit escape
+                screen.Close();
+            if (event.Type == Event::Resized) //resize GL viewport if needed
+                glViewport(0, 0, event.Size.Width, event.Size.Height);
+        }
+        screen.SetActive();//necessary if there's ever multiple windows
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //clear everything out
+
+        glBegin(GL_TRIANGLES); //draw a nice triangle
+            glColor3f(1.0,0.0,0.0);//red
+            glVertex2f(0.50, 0.75);
+            glColor3f(0.0,1.0,0.0);//green
+            glVertex2f(0.25, 0.25);
+            glColor3f(0.0,0.0,1.0);//blue
+            glVertex2f(0.75, 0.25);
+        glEnd();
+        glFlush();//send 'er over to SFML for drawing
+        screen.Display();//draw everything to the screen
+    }
     return 0;
 }
--- README.txt
+++ README.txt
@@ -1,2 +1,13 @@
 Required libraries:
-sdl-1.2.15
+sfml-1.6
+
+Search directories: 
+-Compiler: SFML-1.6\include
+-Linker: SFML-1.6\lib
+
+Project linker settings:
+-lsfml-graphics
+-lsfml-window
+-lsfml-system
+-lopengl32
+-lglu32
